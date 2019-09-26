88bdf7563a5d35624b022c8110107a865771c1bc
sergey <desyak@ya.ru>
2019-09-26 14:28:49 +0300

    готово

--- 6l_ДесякСергей.playground/Contents.swift
+++ 6l_ДесякСергей.playground/Contents.swift
@@ -3,14 +3,24 @@ import UIKit
 struct queue<T: Comparable> {
     var ourColl = [T]()
     
-    mutating func filter () {
-        
+    mutating func filter (item: T) {
+        print("Найдено \(ourColl.filter{$0 == item})")
     }
     
-    subscript(index: Int) -> Int? {
-        if (index < ourColl.lastIndex(where: <#T##(Equatable) throws -> Bool#>))
-    return nil
+    mutating func remove(item: Int){
+        ourColl.remove(at: item)
+    }
+    
+    subscript(index: Int) -> String {
+        return (index < ourColl.count ? ourColl[index] as! String : "nil")
     }
 }
+
+var new = queue<String>(ourColl: ["first", "second"])
+print(new)
+new.filter(item: "first")
+new.remove(item: 1)
+print(new)
+print(new[1])
 
 
